faa9e5d0c444a5721086de37d410bdd3bb29c579
benny <bennycortese@gmail.com>
2023-03-21 00:14:36 -0400

    Added randomish character generator for 5e

--- bot_files/src/main.py
+++ bot_files/src/main.py
@@ -230,6 +230,14 @@ def main(image=bot_image):
                     await message.channel.send(chatgpt_output[:1999])
                     chatgpt_output = chatgpt_output[1999:]
 
+            if message.content.startswith('$random 5e character level'):
+                character_level = message.content[27:]
+                prompt_text = "Return the DnD 5e stat block for a random character of level " + character_level
+                chatgpt_output = complete_text.call(prompt_text)
+                while (len(chatgpt_output) > 0):
+                    await message.channel.send(chatgpt_output[:1999])
+                    chatgpt_output = chatgpt_output[1999:]
+
             if (result := re.match(stock_pattern, message.content)) is not None:
                 stock_name = message.content[7:].upper()
                 stock = yf.download(tickers=stock_name, period='1d', interval='1m')
