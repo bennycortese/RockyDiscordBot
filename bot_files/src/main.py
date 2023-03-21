fac458c014a6490b12d819ba91b5f2944910eaec
benny <bennycortese@gmail.com>
2023-03-21 00:08:44 -0400

    Added statblock retriever for dnd5e

--- bot_files/src/main.py
+++ bot_files/src/main.py
@@ -222,6 +222,14 @@ def main(image=bot_image):
                     await message.channel.send(chatgpt_output[:1999])
                     chatgpt_output = chatgpt_output[1999:]
 
+            if message.content.startswith('$5eStats'):
+                prompt_text = message.content[9:]
+                prompt_text = "Return the DnD 5e stat block for: " + prompt_text
+                chatgpt_output = complete_text.call(prompt_text)
+                while (len(chatgpt_output) > 0):
+                    await message.channel.send(chatgpt_output[:1999])
+                    chatgpt_output = chatgpt_output[1999:]
+
             if (result := re.match(stock_pattern, message.content)) is not None:
                 stock_name = message.content[7:].upper()
                 stock = yf.download(tickers=stock_name, period='1d', interval='1m')
