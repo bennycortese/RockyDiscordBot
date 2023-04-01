9cb77096175a633a01ce6821ef48285e6217adb7
benny <bennycortese@gmail.com>
2023-03-31 23:52:43 -0400

    Random rock

--- bot_files/src/main.py
+++ bot_files/src/main.py
@@ -293,6 +293,13 @@ def main(image=bot_image):
                     await message.channel.send(chatgpt_output[:1999])
                     chatgpt_output = chatgpt_output[1999:]
 
+            if message.content.startswith('$rock'):
+                prompt_text = "give me a type of rock"
+                chatgpt_output = complete_text.call(prompt_text)
+                while (len(chatgpt_output) > 0):
+                    await message.channel.send(chatgpt_output[:1999])
+                    chatgpt_output = chatgpt_output[1999:]
+
             if (result := re.match(stock_pattern, message.content)) is not None:
                 stock_name = message.content[7:].upper()
                 stock = yf.download(tickers=stock_name, period='1d', interval='1m')
